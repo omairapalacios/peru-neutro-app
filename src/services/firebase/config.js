100ceaf0b2b08649fcaef1321598467f898b6ce0
yud-cumba <yudith.cumba@gmail.com>
2021-08-19 17:12:37 -0500

    feat(AddFingerprint): Add initial card using mock, add svg yo forms mock

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCnEIXsuPHfmvhdzyugOxVwFaSMpap2-NY",
-  authDomain: "peru-neutro.firebaseapp.com",
-  databaseURL: "https://peru-neutro-default-rtdb.firebaseio.com",
-  projectId: "peru-neutro",
-  storageBucket: "peru-neutro.appspot.com",
-  messagingSenderId: "401782634973",
-  appId: "1:401782634973:web:19945e07bf3aad398f5e34",
-  measurementId: "G-RG3RTF2VDW"
+const firebaseConfig = {
+  apiKey: 'AIzaSyCnEIXsuPHfmvhdzyugOxVwFaSMpap2-NY',
+  authDomain: 'peru-neutro.firebaseapp.com',
+  databaseURL: 'https://peru-neutro-default-rtdb.firebaseio.com',
+  projectId: 'peru-neutro',
+  storageBucket: 'peru-neutro.appspot.com',
+  messagingSenderId: '401782634973',
+  appId: '1:401782634973:web:19945e07bf3aad398f5e34',
+  measurementId: 'G-RG3RTF2VDW',
 };
 
 // Initialize Firebase
