0747c35c5becc546be8c3ede1e78f7e4c5dda168
yud-cumba <yudith.cumba@gmail.com>
2021-08-19 16:18:01 -0500

    feat(MyAdvances): Add accumulate graphic witch values

--- src/views/MyAdvances/Accumulate.vue
+++ src/views/MyAdvances/Accumulate.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-
+ <div class="d-flex justify-center pa-5 my-3">
+    <div class="semicircle d-flex">
+        <v-img src="@/assets/images/accumulate1.svg"
+        max-width="60" max-height="60"
+        class="img1"/>
+        <v-img src="@/assets/images/accumulate2.svg"
+        max-width="60" class="img2"  max-height="60"/>
+        <v-img src="@/assets/images/accumulate3.svg"
+        max-width="60"  class="img3" max-height="60"/>
+        <div class="value-center text-center">
+            <h1>100</h1>
+            <h1>Agosto</h1>
+        </div>
     </div>
+ </div>
 </template>
 
 <script>
@@ -11,5 +23,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/styles/_variables';
 
+.semicircle {
+    border: 5px solid $color-primary-light;
+    height: 160px;
+    border-top-left-radius: 50% 100%;
+    border-top-right-radius: 50% 100%;
+    width: 75vw;
+}
+%img-general{
+    border: 3px solid $color-primary-light;
+    position: relative;
+    border-radius: 50%;
+}
+.img1 {
+    @extend %img-general;
+    top: 120px;
+    right: 20px;
+}
+.img2 {
+     @extend %img-general;
+    top: 120px;
+    left: 50vw
+}
+.img3 {
+     @extend %img-general;
+    bottom: 30px;
+}
+.value-center{
+    position: relative;
+    top: 60px;
+    right: 60px;
+}
 </style>
--- src/views/MyAdvances/Index.vue
+++ src/views/MyAdvances/Index.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
-        Avances
+        <v-card class="pa-5">
+            <h1>Hola Jeff</h1>
+            <Accumulate/>
+        </v-card>
     </div>
 </template>
 
 <script>
 export default {
+  components: {
+    Accumulate: () => import('./Accumulate.vue'),
+  },
 };
 </script>
 
