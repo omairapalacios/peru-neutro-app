fd3aa7d65eb8910056027b58a9c26d8d27ce1b79
yud-cumba <yudith.cumba@gmail.com>
2021-08-19 23:48:31 -0500

    feat(Home): modify disabled card depends to preferences

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -7,17 +7,21 @@
       </p>
     </v-row>
     <v-row class="d-flex flex-wrap justify-center">
-      <Section v-for="section in forms" :key="section.formId" :section="section" />
+      <Section v-for="section in forms" :key="section.formId"
+      :section="section" :disabled="preferencesFormId.includes(section.formId)"/>
     </v-row>
   </v-container>
 </template>
 
 <script>
 import forms from '@/mocks/forms.json';
+import { getDocumentByQuery } from '@/services/firebase/methods';
 
 export default {
-  created() {
+  name: 'Home',
+  async created() {
     this.$store.commit('SET_LAYOUT', 'main-layout');
+    await this.getMyPreferences();
   },
   components: {
     Section: () => import('./Section.vue'),
@@ -25,7 +29,15 @@ export default {
   data() {
     return {
       forms,
+      preferencesFormId: [],
+      userId: 'EtKqyTS22jPkBG5swy1l95BXS2Z2',
     };
+  },
+  methods: {
+    async getMyPreferences() {
+      const preferences = await getDocumentByQuery('PREFERENCES', { key: 'userId', value: this.userId });
+      this.preferencesFormId = preferences.map((e) => e.formId);
+    },
   },
 };
 </script>
