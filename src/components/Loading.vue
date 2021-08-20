abb7fc8ed2f9c03052e482297f0c133d79ec4dc0
yud-cumba <yudith.cumba@gmail.com>
2021-08-20 12:59:04 -0500

    feat(Loading) Add component loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="loading-view d-flex flex-column justify-center align-center">
+    <DotLoader
+      class="ring-loader"
+      color="green"
+      :loading="loading"
+      :size="60"
+      sizeUnit="px"
+    />
+    <p class="mt-4 font-weight-thin"><strong>Cargando, por favor espere un momento...</strong></p>
+  </div>
+</template>
+
+<script>
+import { DotLoader } from '@saeris/vue-spinners';
+
+export default {
+  components: { DotLoader },
+  props: {
+    loading: {
+      type: Boolean,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.loading-view {
+  height: 90vh;
+  width: 100%;
+}
+</style>
