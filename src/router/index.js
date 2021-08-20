690797cfa862094c1d44c96860a941840a6a8ac2
yud-cumba <yudith.cumba@gmail.com>
2021-08-20 15:15:12 -0500

    feat(Forms): add fingerpint

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ const routerOptions = [
   { path: '/certificate', name: 'RequestCertificate' },
   { path: '/advances', name: 'MyAdvances' },
   { path: '/advances', name: 'MyAdvances' },
-  { path: '/advance/:id', name: 'AdvanceDetail' },
+  { path: '/advance/:sectionId', name: 'AdvanceDetail' },
   { path: '/addfingerprint/:sectionId', name: 'AddFingerprint' },
   { path: '*', name: 'NotFound' },
 ];
