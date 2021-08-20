568f056e901ea9a5ab994b409414509418c5144b
Maray Montes de Oca <maraymontes@gmail.com>
2021-08-20 12:18:01 -0500

    AdvanceDetail

--- src/views/AdvanceDetail/Index.vue
+++ src/views/AdvanceDetail/Index.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
-    <Toolbar title="Transporte" :goBack="goBack" />
+    <Toolbar :title="currentAdvanceDetail.title" :goBack="goBack" />
+    <AdvanceByMonth/>
+    <CarbonEmissions porcentage='0.28'/>
+    <RegisterConsume :answer="currentAdvanceDetail.answers"/>
   </div>
 </template>
 
 <script>
+import data from '@/mocks/answers.json';
+
 export default {
+  data: () => ({
+    currentAdvanceDetail: [],
+  }),
   components: {
     Toolbar: () => import('../../components/ToolBar.vue'),
+    AdvanceByMonth: () => import('./AdvanceByMonth.vue'),
+    CarbonEmissions: () => import('./CarbonEmissions.vue'),
+    RegisterConsume: () => import('./RegisterConsume.vue'),
+  },
+  created() {
+    console.log(this.$route.params);
+    [this.currentAdvanceDetail] = data.filter((e) => e.sectionId === this.$route.params.id);
+    console.log(this.currentAdvanceDetail.answers);
   },
   methods: {
     goBack() {
@@ -16,6 +32,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-</style>
