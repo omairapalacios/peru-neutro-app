1c941b1e35b53d929980d4d26aee086d9aa855ee
omairapalacios <cpalaciosanchez@hotmail.com>
2021-08-20 17:51:40 -0500

    feat(addfootprint) : register carbon footprint

--- src/views/AddFingerprint/Index.vue
+++ src/views/AddFingerprint/Index.vue
@@ -2,83 +2,101 @@
   <div>
     <Toolbar title="Registrar" />
     <div class="pa-5">
-    <label
-      class="align-self-start subtitle-2 font-weight-bold grey-darken-4--text text-uppercase"
-      for="m_lastname"
-      >Fecha
-    </label>
-    <v-text-field
-      class="rounded-lg"
-      outlined
-      dense
-      type="date"
-      name="birthday"
-      placeholder="dd/mm/yyyy"
-      v-model.trim="date"
-      :rules="[v => !!v || 'el campo es requerido']"
-    ></v-text-field>
-    <div v-for="q in section.questions" :key="q.questionId">
       <label
         class="align-self-start subtitle-2 font-weight-bold grey-darken-4--text text-uppercase"
         for="m_lastname"
-        >{{ q.question }}
+        >Fecha
       </label>
       <v-text-field
         class="rounded-lg"
         outlined
         dense
-        placeholder="1000"
-        :rules="[v => !!v || 'el campo es requerido']"
+        type="date"
+        name="birthday"
+        placeholder="dd/mm/yyyy"
+        v-model.trim="date"
+        :rules="[(v) => !!v || 'el campo es requerido']"
       ></v-text-field>
-    </div>
+      <div ref="question" v-for="q in section.questions" :accesskey="q.questionId" :key="q.questionId">
+        <label
+          class="align-self-start subtitle-2 font-weight-bold grey-darken-4--text text-uppercase"
+          for="m_lastname"
+          >{{ q.question }}- {{q.questionId}}
+        </label>
+        <v-text-field
+          class="rounded-lg"
+          outlined
+          dense
+          v-model.number="value"
+          placeholder="1000"
+          :rules="[(v) => !!v || 'el campo es requerido']"
+        ></v-text-field>
+      </div>
     </div>
     <v-row class="mt-auto d-flex justify-center align-center">
-      <v-btn class="btn-dark">
-        Registrar
-      </v-btn>
+      <v-btn @click="addFootprint" class="btn-dark"> Registrar </v-btn>
     </v-row>
   </div>
 </template>
 
 <script>
-import sections from '@/mocks/sections.json';
+import { mapState } from "vuex";
+import sections from "@/mocks/sections.json";
+import { addDocument } from "@/services/firebase/methods";
+import dayjs from "dayjs";
 
 export default {
   components: {
-    Toolbar: () => import('../../components/ToolBar.vue'),
-  },
-  data: () => ({
-    section: {},
-    answer: {
-      userId: '1234567890',
-      sectionId: 'sectionTransportGasohol01',
-      miliseconds: '1629258671',
-      user_date: '1234567890-1629258671',
-      answers: [
-        {
-          questionId: 'TransportGasohol01Q1',
-          value: '1000',
-          status: 'registrado',
-        },
-      ],
-    },
-    date: 0,
-    answerData: {
-      userId: '1234567890',
-      sectionId: 'sectionTransportGasohol01',
-      miliseconds: '1629258671',
-      user_date: '1234567890-1629258671',
-    },
-  }),
-  created() {
-    // mock getSectionById
-    [this.section] = sections.filter((e) => e.sectionId === this.sectionId);
+    Toolbar: () => import("../../components/ToolBar.vue"),
   },
   computed: {
+    ...mapState(["user"]),
     sectionId() {
       return this.$route.params.sectionId;
     },
   },
+  data: () => ({
+    section: {},
+    value: "",
+    date: "",
+    answer: {
+      userId: "",
+      sectionId: "",
+      date: 0,
+      milliseconds_created: dayjs().valueOf(),
+      user_date: "",
+      user_section: '',
+      answers: [],
+    },
+    date: 0,
+    answerData: {
+      userId: "1234567890",
+      sectionId: "sectionTransportGasohol01",
+      milliseconds: "1629258671",
+      user_date: "1234567890-1629258671",
+    },
+  }),
+  created() {
+    // mock getSectionById
+    console.log(this.user, this.answer);
+    [this.section] = sections.filter((e) => e.sectionId === this.sectionId);
+  },
+  methods:{
+    addFootprint() {
+      this.answer.userId = this.user.userId;
+      this.answer.date = dayjs(this.date).valueOf();
+      this.answer.user_date = `${this.user.userId}-${dayjs(this.date).valueOf()}`;
+      this.answer.sectionId = this.sectionId;
+      this.answer.user_section =`${this.user.userId}-${this.sectionId}`;
+      this.answer.answers.push({
+        value: this.value,
+        questionId: this.section.questions[0].questionId,
+        status:'registrado'
+      })
+      addDocument("ANSWERS", this.answer);
+      this.$router.go(-1);
+    }
+  }
 };
 </script>
 
