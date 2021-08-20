8c78267b7d301e85aa0af4fff1cc06e4d006aa3d
yud-cumba <yudith.cumba@gmail.com>
2021-08-20 16:39:36 -0500

    feat(Answer): add value fingerprint total, and date of realtime database

--- src/views/AdvanceDetail/AdvanceByMonth.vue
+++ src/views/AdvanceDetail/AdvanceByMonth.vue
@@ -23,11 +23,29 @@
 </template>
 
 <script>
-
+/* eslint-disable no-param-reassign */
 export default {
   data: () => ({
     listOfMonths: ['Ago', 'Sep', 'Oct', 'Nov', 'Dic', 'Ene', 'Feb'],
   }),
+  methods: {
+    msToTime(s) {
+      // Pad to 2 or 3 digits, default is 2
+      function pad(n, z) {
+        z = z || 2;
+        return (`00${n}`).slice(-z);
+      }
+
+      const ms = s % 1000;
+      s = (s - ms) / 1000;
+      const secs = s % 60;
+      s = (s - secs) / 60;
+      const mins = s % 60;
+      const hrs = (s - mins) / 60;
+
+      return `${pad(hrs)}:${pad(mins)}:${pad(secs)}.${pad(ms, 3)}`;
+    },
+  },
 };
 </script>
 
--- src/views/AdvanceDetail/Index.vue
+++ src/views/AdvanceDetail/Index.vue
@@ -2,9 +2,11 @@
   <div>
     <Toolbar title="Avance" :goBack="goBack" />
     <AdvanceByMonth/>
-    <CarbonEmissions porcentage='0.28'/>
+    <CarbonEmissions :fingerprint='fingerprintTotal'/>
     <RegisterConsume/>
-    <v-row justify="space-between" align="center" class="px-4 header text-white">
+    <div v-for="fingerprint in currentAdvanceDetail" :key="fingerprint.user_date">
+    <v-row
+    justify="space-between" align="center" class="px-4 header text-white">
       <v-col cols="6">
         <span>Agosto</span>
       </v-col>
@@ -12,8 +14,9 @@
         <span> GAL</span>
       </v-col>
     </v-row>
-    <div v-for="fingerprint in currentAdvanceDetail" :key="fingerprint.user_date">
-        <Answer :answer="fingerprint"/>
+    <div v-for="answer in fingerprint.answers" :key="answer.questionId">
+        <Answer :answer="answer" :miliseconds="fingerprint.miliseconds"/>
+    </div>
     </div>
   </div>
 </template>
@@ -36,12 +39,25 @@ export default {
   async created() {
     await this.getAnswers();
   },
-  computed: { ...mapState(['user']) },
+  computed: {
+    sectionId() {
+      return this.$route.params.sectionId;
+    },
+    fingerprintTotal() {
+      const answers = this.currentAdvanceDetail.map((e) => e.answers.map((a) => Number(a.value)).reduce((a, b) => a + b, 0));
+      const fingerprintTotal = answers.reduce((a, b) => Number(a) + Number(b), 0);
+      // mock de formula de huella de carbono
+      const fingerprint = fingerprintTotal / 5000;
+      //
+      return fingerprint;
+    },
+    ...mapState(['user']),
+  },
   methods: {
     async getAnswers() {
       this.currentAdvanceDetail = await getDocumentByQuery('ANSWERS', {
-        key: 'userId',
-        value: this.user.userId,
+        key: 'user-section',
+        value: `${this.user.userId}-${this.sectionId}`,
       });
     },
     goBack() {
